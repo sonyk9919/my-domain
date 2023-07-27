6f81742fc952d295987a43b54b634d1b0d81d7d6
sonyk9919 <sonyk9919@gmail.com>
2023-07-27 14:38:10 +0900

    chore: SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import RootLayoutProvider from "./components/RootLayoutProvider";
+
+export const metadata: Metadata = {
+  title: "sonyk9919의 개인 페이지",
+  description: "아무거나 잡다한거 올리는 공간 ㅋㅋ",
+};
 
 export default function RootLayout({
   children,
